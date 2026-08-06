9bb2563daa5d7cdff8e75162dcdc3e1b3ca9f8f7
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 18:41:23 +0200

    Update ClubRepository.cs

--- Repositories/ClubRepository.cs
+++ Repositories/ClubRepository.cs
@@ -15,16 +15,25 @@ public class ClubRepository : IClubRepository
 
     public async Task<Club?> GetClubAsync()
     {
-        return await _context.Clubs.FirstOrDefaultAsync();
+        return await _context.Clubs
+            .Include(c => c.Players)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<bool> ClubExistsAsync()
+    {
+        return await _context.Clubs.AnyAsync();
     }
 
     public async Task SaveClubAsync(Club club)
     {
-        if (club.Id == 0)
-            _context.Clubs.Add(club);
-        else
-            _context.Clubs.Update(club);
+        await _context.Clubs.AddAsync(club);
+        await _context.SaveChangesAsync();
+    }
 
+    public async Task UpdateClubAsync(Club club)
+    {
+        _context.Clubs.Update(club);
         await _context.SaveChangesAsync();
     }
 }
