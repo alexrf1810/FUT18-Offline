f4d7953a66b716f77e82c607624b41fcc6aeb0fd
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 15:19:18 +0200

    Create Club.cs

---
+++ Models/Club.cs
@@ -0,0 +1,14 @@
+namespace FUT18Launcher.Models;
+
+public class Club
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public long Coins { get; set; }
+
+    public int Level { get; set; }
+
+    public int Experience { get; set; }
+}
