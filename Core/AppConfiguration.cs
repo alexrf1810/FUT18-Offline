c4746626da19232d146bc8d7c653165cd369f334
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 15:17:14 +0200

    Create AppConfiguration.cs

---
+++ Core/AppConfiguration.cs
@@ -0,0 +1,10 @@
+namespace FUT18Launcher.Core;
+
+public static class AppConfiguration
+{
+    public const string DatabaseFile = "fut18offline.db";
+
+    public const string SaveFolder = "Save";
+
+    public const string AssetsFolder = "Assets";
+}
