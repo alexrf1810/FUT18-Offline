4dbb4ce1e332aeb27f832c6c243aeb8e1f2c4275
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 19:10:40 +0200

    Create StoreView.xaml.cs

---
+++ Views/Store/StoreView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace FUT18Launcher.Views.Store;
+
+public partial class StoreView : UserControl
+{
+    public StoreView()
+    {
+        InitializeComponent();
+    }
+}
