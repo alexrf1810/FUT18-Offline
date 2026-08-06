161fc5cd9ec9716fc53ab4e6847be47dca0c36e2
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 18:59:34 +0200

    Create ShellView.xaml.cs

---
+++ Views/Shell/ShellView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace FUT18Launcher.Views.Shell;
+
+public partial class ShellView : UserControl
+{
+    public ShellView()
+    {
+        InitializeComponent();
+    }
+}
