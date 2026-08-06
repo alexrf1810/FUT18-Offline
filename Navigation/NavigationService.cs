11ac7e7212967ed8ce40501f9fb6a9f57170b9d7
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 15:18:59 +0200

    Create NavigationService.cs

---
+++ Navigation/NavigationService.cs
@@ -0,0 +1,9 @@
+namespace FUT18Launcher.Navigation;
+
+public class NavigationService : INavigationService
+{
+    public void NavigateTo<TViewModel>()
+    {
+        // Se implementará en el siguiente bloque.
+    }
+}
