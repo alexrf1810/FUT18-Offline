eb1afee045595eb2196248139d99e3310e9e4d05
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 18:30:28 +0200

    Update Club.cs

--- Models/Club.cs
+++ Models/Club.cs
@@ -1,14 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FUT18Launcher.Models;
 
 public class Club
 {
     public int Id { get; set; }
 
+    [Required]
+    [MaxLength(64)]
     public string Name { get; set; } = string.Empty;
 
-    public long Coins { get; set; }
+    [Required]
+    [MaxLength(64)]
+    public string ManagerName { get; set; } = string.Empty;
 
-    public int Level { get; set; }
+    public int Coins { get; set; } = 500;
 
-    public int Experience { get; set; }
+    public int Level { get; set; } = 1;
+
+    public int Experience { get; set; } = 0;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime LastLogin { get; set; } = DateTime.UtcNow;
+
+    public int BadgeId { get; set; }
+
+    public int StadiumId { get; set; }
+
+    public int BallId { get; set; }
+
+    public int HomeKitId { get; set; }
+
+    public int AwayKitId { get; set; }
+
+    public ICollection<Player> Players { get; set; } = new List<Player>();
 }
