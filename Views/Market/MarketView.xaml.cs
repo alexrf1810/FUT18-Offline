8313cf96ba1fa6ad0b8c4fe1484230624dc53df0
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 19:11:34 +0200

    Create MarketView.xaml.cs

---
+++ Views/Market/MarketView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace FUT18Launcher.Views.Market;
+
+public partial class MarketView : UserControl
+{
+    public MarketView()
+    {
+        InitializeComponent();
+    }
+}
