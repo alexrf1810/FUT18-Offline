b38be346b19654c4a4b3fd1a1dfff47ac6da76f9
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 15:42:07 +0200

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,28 @@
 using System.Windows;
+using Microsoft.Extensions.Hosting;
+using FUT18Launcher.Core;
 
 namespace FUT18Launcher;
 
 public partial class App : Application
 {
+    public static IHost Host { get; private set; } = null!;
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        Host = ApplicationHost.BuildHost();
+
+        Host.Start();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await Host.StopAsync();
+
+        Host.Dispose();
+
+        base.OnExit(e);
+    }
 }
