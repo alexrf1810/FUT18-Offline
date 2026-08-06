f75f0197b3b47433081b012abbdbcff0e41ce512
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 19:08:19 +0200

    Create HomeView.xaml.cs

---
+++ Views/Home/HomeView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace FUT18Launcher.Views.Home;
+
+public partial class HomeView : UserControl
+{
+    public HomeView()
+    {
+        InitializeComponent();
+    }
+}
