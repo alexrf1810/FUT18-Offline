0f16b91aa00e752e248f259f474d91be94ddb419
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 19:05:39 +0200

    Update ShellViewModel.cs

--- ViewModels/Shell/ShellViewModel.cs
+++ ViewModels/Shell/ShellViewModel.cs
@@ -1,11 +1,25 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using FUT18Launcher.Navigation;
+using FUT18Launcher.ViewModels.Club;
+using FUT18Launcher.ViewModels.Home;
+using FUT18Launcher.ViewModels.Market;
+using FUT18Launcher.ViewModels.SBC;
+using FUT18Launcher.ViewModels.Squad;
+using FUT18Launcher.ViewModels.Store;
 
 namespace FUT18Launcher.ViewModels.Shell;
 
 public partial class ShellViewModel : BaseViewModel
 {
     private readonly NavigationStore _navigationStore;
+
+    private readonly HomeViewModel _homeViewModel;
+    private readonly SquadViewModel _squadViewModel;
+    private readonly StoreViewModel _storeViewModel;
+    private readonly ClubViewModel _clubViewModel;
+    private readonly MarketViewModel _marketViewModel;
+    private readonly SbcViewModel _sbcViewModel;
 
     [ObservableProperty]
     private string clubName = string.Empty;
@@ -21,9 +35,23 @@ public partial class ShellViewModel : BaseViewModel
 
     public BaseViewModel CurrentViewModel => _navigationStore.CurrentViewModel;
 
-    public ShellViewModel(NavigationStore navigationStore)
+    public ShellViewModel(
+        NavigationStore navigationStore,
+        HomeViewModel homeViewModel,
+        SquadViewModel squadViewModel,
+        StoreViewModel storeViewModel,
+        ClubViewModel clubViewModel,
+        MarketViewModel marketViewModel,
+        SbcViewModel sbcViewModel)
     {
         _navigationStore = navigationStore;
+
+        _homeViewModel = homeViewModel;
+        _squadViewModel = squadViewModel;
+        _storeViewModel = storeViewModel;
+        _clubViewModel = clubViewModel;
+        _marketViewModel = marketViewModel;
+        _sbcViewModel = sbcViewModel;
 
         _navigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
     }
@@ -43,5 +71,41 @@ public partial class ShellViewModel : BaseViewModel
         Coins = coins;
         FIFAPoints = fifaPoints;
         Level = level;
+    }
+
+    [RelayCommand]
+    private void OpenHome()
+    {
+        _navigationStore.CurrentViewModel = _homeViewModel;
+    }
+
+    [RelayCommand]
+    private void OpenSquad()
+    {
+        _navigationStore.CurrentViewModel = _squadViewModel;
+    }
+
+    [RelayCommand]
+    private void OpenStore()
+    {
+        _navigationStore.CurrentViewModel = _storeViewModel;
+    }
+
+    [RelayCommand]
+    private void OpenClub()
+    {
+        _navigationStore.CurrentViewModel = _clubViewModel;
+    }
+
+    [RelayCommand]
+    private void OpenMarket()
+    {
+        _navigationStore.CurrentViewModel = _marketViewModel;
+    }
+
+    [RelayCommand]
+    private void OpenSbc()
+    {
+        _navigationStore.CurrentViewModel = _sbcViewModel;
     }
 }
