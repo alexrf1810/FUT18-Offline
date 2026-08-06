c2f06c3e6959c4d8b8a7a771214c2855c64aa552
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 19:12:00 +0200

    Create SbcView.xaml.cs

---
+++ Views/SBC/SbcView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace FUT18Launcher.Views.SBC;
+
+public partial class SbcView : UserControl
+{
+    public SbcView()
+    {
+        InitializeComponent();
+    }
+}
