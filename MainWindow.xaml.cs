352e5c61a398490a8fdb903c132b42205fbfad2b
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 15:28:00 +0200

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using FUT18Launcher.ViewModels;
 
 namespace FUT18Launcher;
 
@@ -7,5 +8,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainViewModel();
     }
 }
