a4fa335f8c012be22a90c642a1475ac375c2dc36
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 16:13:38 +0200

    Update ServiceRegistration.cs

--- Services/ServiceRegistration.cs
+++ Services/ServiceRegistration.cs
@@ -1,5 +1,6 @@
-using Microsoft.Extensions.DependencyInjection;
 using FUT18Launcher.Navigation;
+using FUT18Launcher.Repositories;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FUT18Launcher.Services;
 
@@ -8,6 +9,8 @@ public static class ServiceRegistration
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddSingleton<INavigationService, NavigationService>();
+
+        services.AddScoped<IClubRepository, ClubRepository>();
 
         return services;
     }
