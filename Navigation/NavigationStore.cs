d07295e07740be9214f3b456c42fccff2ed53466
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 16:25:52 +0200

    Create NavigationStore.cs

---
+++ Navigation/NavigationStore.cs
@@ -0,0 +1,20 @@
+using FUT18Launcher.ViewModels;
+
+namespace FUT18Launcher.Navigation;
+
+public class NavigationStore
+{
+    private BaseViewModel? _currentViewModel;
+
+    public BaseViewModel? CurrentViewModel
+    {
+        get => _currentViewModel;
+        set
+        {
+            _currentViewModel = value;
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+
+    public event Action? CurrentViewModelChanged;
+}
