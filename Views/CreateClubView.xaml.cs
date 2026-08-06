6c983406312ec2c458a7adcebd6536d28e02117a
alexrf1810 <alexrf1810@gmail.com>
2026-08-06 16:18:55 +0200

    Create CreateClubView.xaml.cs

---
+++ Views/CreateClubView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace FUT18Launcher.Views;
+
+public partial class CreateClubView : UserControl
+{
+    public CreateClubView()
+    {
+        InitializeComponent();
+    }
+}
